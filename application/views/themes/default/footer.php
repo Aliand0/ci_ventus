b5a9a69ec8069817921c0282cf9b4d0b0982286d
Aliand0 <q.s.ali.imron@gmail.com>
2018-05-09 10:47:41 +0700

    Aliand0

---
+++ application/views/themes/default/footer.php
@@ -0,0 +1,21 @@
+<footer class="footer-wrap" id="footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12">
+              <div class="clearfix footer-inner">
+                
+                <div class="copyright">
+                <ul class="social-icons">
+                  <li><a data-tooltip="Facebook" data-delay="50"  data-toggle="tooltip" data-position="top" class="tooltips tooltipped facebook" href="#"><i class="fa fa-facebook"></i></a>
+                  </li>
+                  <li><a data-tooltip="Twitter" data-delay="50" data-toggle="tooltip"  data-position="top" class="tooltips tooltipped twitter" href="#"><i class="fa fa-twitter"></i></a>
+                  </li>
+                  <li><a data-tooltip="Google Plus" data-delay="50" data-toggle="tooltip" data-position="top" class="tooltips tooltipped google-plus" href="#"><i class="fa fa-google-plus"></i></a>
+                  </li>
+                </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
---
+++ application/views/themes/default/header.php
@@ -0,0 +1,33 @@
+
+<div class="navbar navbar-default navbar-fixed-top top-navbar navbar-transparent">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="<?php echo site_url('#')?>"><h1 class="logo">VENTUS</h1></a>
+            </div>
+            <div class="navbar-collapse collapse navbar-responsive-collapse">
+
+              <ul class="nav navbar-nav navbar-right">
+                <?php echo $main_menus;?>
+              </ul>
+            </div>
+          </div>
+      </div>
+      <section id="header">
+        <div class="hero">
+          <div class="container">
+            <div class="welcome text-center">
+            <font color="black">
+              <h1>VENTUS</h1>
+              <h2>Selamat Datang <br/> 
+              <strong>Post Event Kamu Sekarang</strong></h2>
+              </font>
+              <a class="btn btn-success btn-lg" href="/ci-ventus/index.php/admin/posts/add">Create Event</a>
+            </div>
+          </div>
+        </div>
+      </section>
